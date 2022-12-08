76827dd122c495e83706e4b7bbabd46f5da13397
Rizwan Ali <rizwan.ali@sumatosolutions.com>
2022-12-08 19:14:49 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rizwan Ali BCSM-F19-178
+          Rizwan Ali BCSM-F19-178 7A
         </p>
         <a
           className="App-link"
