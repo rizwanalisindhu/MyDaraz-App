9b825c596627bdc675fd0edc7fdd58de2dd55fc3
Rizwan Ali <rizwan.ali@sumatosolutions.com>
2022-12-08 22:39:13 +0500

    Add product Listing (tital,category,price,price), and add "Add to cart" button

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,25 @@
 import '../App.css';
 
-function ProductList(){
-    return(
-        <p>product</p>
+function ProductList({ product }) {
+    return (
+        <div className='flex'>
+            {product.map((productItem, productIndex) => {
+                return (
+                    <div style={{ width: '33%' }}>
+
+                        <div className='product-item'>
+                            <img src={productItem.url} width="100%" />
+                            <p>{productItem.name} | {productItem.category}</p>
+                            <p>{productItem.seller}</p>
+                            <p>Rs. {productItem.price} /-</p>
+                            <button>Add To Cart</button>
+                        </div>
+
+                    </div>
+                )
+            })
+            }
+        </div>
     )
 }
 
