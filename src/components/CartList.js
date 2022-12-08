b304a144f4145724165bcc24ae22f4338685e295
Rizwan Ali <rizwan.ali@sumatosolutions.com>
2022-12-09 00:28:01 +0500

    implementation of + and - product quantity

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,25 +1,43 @@
 import '../App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 function CartList({ cart }) {
-    return (
 
+    const [CART, setCART] = useState([])
+
+    useEffect(() => {setCART(cart)}, [cart])
+
+    return (
         <div>
             {
-                cart.map((cartItem, cartindex) => {
+                CART?.map((cartItem, cartindex) => {
                     return (
                         <div>
                             <img src={cartItem.url} width={40} />
                             <span> {cartItem.name} </span>
                             <button> -</button>
                             <span> {cartItem.quantity} </span>
-                            <button>+ </button>
+                            <button 
+                            onClick={() => {
+                                const _CART = CART.map((item, index) => {
+                                    return cartindex === index ? { ...item, quantity: item.quantity + 1 } : item
+                                })
+                                setCART(_CART)
+                            }}
+                            >+ </button>
 
-                            <span> {cartItem.price} </span>
+                            <span> {cartItem.price * cartItem.quantity} </span>
                         </div>
                     )
                 })
             }
-           
+            <p> Total : <span></span>
+                {
+                    CART.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
+                }
+            </p>
 
         </div>
     )
