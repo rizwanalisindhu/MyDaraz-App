8a273b04cae26b8ea46400e236c4c2b0670fec8c
Rizwan Ali <rizwan.ali@sumatosolutions.com>
2022-12-09 00:39:43 +0500

    improve look and feel of CartList

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -7,33 +7,44 @@ function CartList({ cart }) {
 
     const [CART, setCART] = useState([])
 
-    useEffect(() => {setCART(cart)}, [cart])
+    useEffect(() => { setCART(cart) }, [cart])
 
     return (
         <div>
             {
                 CART?.map((cartItem, cartindex) => {
                     return (
-                        <div>
-                            <img src={cartItem.url} width={40} />
+                        <div className='cartlist-item'>
+                            <div>
+                                <img src={cartItem.url} width={40} />
+                            </div>
+                            <div>
                             <span> {cartItem.name} </span>
-                            <button> -</button>
+                            <button
+                                onClick={() => {
+                                    const _CART = CART.map((item, index) => {
+                                        return cartindex === index ? { ...item, quantity: item.quantity > 0 ? item.quantity - 1 : 0 } : item
+                                    })
+                                    setCART(_CART)
+                                }}
+                            > -</button>
                             <span> {cartItem.quantity} </span>
-                            <button 
-                            onClick={() => {
-                                const _CART = CART.map((item, index) => {
-                                    return cartindex === index ? { ...item, quantity: item.quantity + 1 } : item
-                                })
-                                setCART(_CART)
-                            }}
+                            <button
+                                onClick={() => {
+                                    const _CART = CART.map((item, index) => {
+                                        return cartindex === index ? { ...item, quantity: item.quantity + 1 } : item
+                                    })
+                                    setCART(_CART)
+                                }}
                             >+ </button>
 
                             <span> {cartItem.price * cartItem.quantity} </span>
                         </div>
+                        </div>
                     )
                 })
             }
-            <p> Total : <span></span>
+            <p> <b>Total : </b><span></span>
                 {
                     CART.map(item => item.price * item.quantity).reduce((total, value) => total + value, 0)
                 }
